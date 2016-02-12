57c37f8d5cf27110acc9f8eea0c7955ac61a93dd
Ivan <ivan.baena04@gmail.com>
2016-02-12 03:39:30 -0500

    created model folder and burger.js

--- config/orm.js
+++ config/orm.js
@@ -2,13 +2,14 @@
 var mysql = require("mysql");
 var dBconn = require("./connection.js");
 
-//Select all data from Burges Table
-dBconn.query('SELECT * FROM Burgers',function(err,res){
-  if(err) throw err;
+//Select all data from Burges Table and show in terminal
+dBconn.query('SELECT * FROM Burgers', function(err, res) {
+  if (err) throw err;
 
   console.log('Data received from Db:\n');
   console.log(res);
 });
+
 //Create New burger
 //var newBurger = { burger_name: 'Bacon Burger', devoured: false };
 dBconn.query('INSERT INTO Burgers SET ?', newBurger, function(err,res){
@@ -16,3 +17,16 @@ dBconn.query('INSERT INTO Burgers SET ?', newBurger, function(err,res){
 
   console.log('Last insert ID:', res);
 });
+
+//Change devoured status
+var bool = true;
+dBconn.query(
+  'UPDATE Burgers SET devoured = ? Where ID = ?', [true, 2],
+  function(err, result) {
+    if (err) throw err;
+
+    console.log('Changed ' + result.changedRows + ' rows');
+  }
+);
+
+module.exports = dBconn;
